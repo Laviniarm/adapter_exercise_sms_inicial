486743ba39460111e94f49aca6ba48a372781472
Lavínia Rodrigues <96793999+Laviniarm@users.noreply.github.com>
2025-03-09 12:37:35 -0300

    Ajuste do main

--- src/AppSMSNotification.java
+++ src/AppSMSNotification.java
@@ -2,30 +2,36 @@ import api.*;
 import api.factory.SMSFactoryOperadora;
 import api.model.SMS;
 
+import java.util.Scanner;
+
 public class AppSMSNotification {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-		SMS message1 = new SMS("83988885544", "4183988221133", "Bom dia. Seu boleto já está disponível para pagamento.");
-		SMS message2 = new SMS("83988885544", "1583988221133", "Bom dia. Seu boleto já está disponível para pagamento / VIVO.");
-		SMS message3 = new SMS("83996488531", "1583988221133", "Good Morning!");
+		System.out.print("Digite o telefone de destino (Formato: DDD + Número): ");
+		String numeroDestino = scanner.nextLine();
 
-		message3.setTranslate(true);
+		System.out.print("Digite a mensagem: ");
+		String texto = scanner.nextLine();
 
-		enviarSMS(message1);
-		enviarSMS(message2);
-		enviarSMS(message3);
-	}
+		System.out.print("Deseja traduzir a mensagem de Inglês para Português? (s/n): ");
+		boolean traduzir = scanner.nextLine().equalsIgnoreCase("s");
 
-	private static void enviarSMS(SMS sms) {
-		try {;
+		try {
 
-			SMSSender sender = SMSFactoryOperadora.getRemetente(sms.getDestino(), sms.getTranslate());
+			SMS sms = new SMS("83988885544", numeroDestino, texto);
+			sms.setTranslate(traduzir);
 
-			boolean sucesso = sender.sendSMS(sms);
-			System.out.println(sucesso ? "SMS enviado com sucesso!" : "Falha no envio do SMS.");
+			SMSSender sender = SMSFactoryOperadora.getRemetente(numeroDestino, traduzir);
+			sender.sendSMS(sms);
 
 		} catch (IllegalArgumentException e) {
 			System.out.println("Erro: " + e.getMessage());
 		}
 	}
 }
+
+
+//		SMS message1 = new SMS("83988885544", "4183988221133", "Bom dia. Seu boleto já está disponível para pagamento.");
+//		SMS message2 = new SMS("83988885544", "1583988221133", "Bom dia. Seu boleto já está disponível para pagamento / VIVO.");
+//		SMS message3 = new SMS("83996488531", "1583988221133", "Good Morning!");
